4c7861fc53677f114726173023ec4a830177726e
Dylan Heathcote <c19434966@mytudublin.ie>
2022-11-06 22:00:28 +0000

    final commit, everything working

--- Lab_2/Problem-1/script.js
+++ Lab_2/Problem-1/script.js
@@ -1,6 +1,7 @@
-window.onload = function(){
+var TEXT_NODE = 3;
+
+function addcontact(){
     //adding click event listener for when a user clicks add contact
-    document.getElementById("addcontact").addEventListener("click", () => {
         //getting the error ID
         document.getElementById("error")
         //getting the value of the Name, number and email from the input boxes
@@ -51,22 +52,74 @@ window.onload = function(){
         cell3.innerHTML = emailText;
 
         //resetting the HTML values to be blank
-        name1.value = "";
+            name1.value = "";
 		    number.value = "";
 		    email.value = "";
-
-    })
-
-    //Displaying the error message function when an error is triggered, it will display whatever specific error.
-    function displayError(message) {
-        document.getElementById("errormessage").innerHTML = message;
-        //Removes the hidden CSS so the error actually displays.
-        document.getElementById("popupmessage").classList.remove("hidden");
     }
-        //adds event listener so when the X is clicked, it adds the hidden CSS to hide the error message.
-	      document.getElementById("closeerror").addEventListener("click", () => {
-		    document.getElementById("popupmessage").classList.add("hidden");
-	})
-    }
-    
-  
+
+            function displayError(message) {
+                document.getElementById("errormessage").innerHTML = message;
+                //Removes the hidden CSS so the error actually displays.
+                document.getElementById("popupmessage").classList.remove("hidden");
+            }
+                //adds event listener so when the X is clicked, it adds the hidden CSS to hide the error message.
+            function closeerror() {
+                document.getElementById("popupmessage").classList.add("hidden");
+            }
+            
+            //source : https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_sort_table_desc couldn't figure it out by myself.
+
+            function sort() {
+                var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+                table = document.getElementById("table");
+                switching = true;
+                //Set the sorting direction to ascending:
+                dir = "asc"; 
+                /*Make a loop that will continue until
+                no switching has been done:*/
+                while (switching) {
+                  //start by saying: no switching is done:
+                  switching = false;
+                  rows = table.rows;
+                  /*Loop through all table rows (except the
+                  first, which contains table headers):*/
+                  for (i = 1; i < (rows.length - 1); i++) {
+                    //start by saying there should be no switching:
+                    shouldSwitch = false;
+                    /*Get the two elements you want to compare,
+                    one from current row and one from the next:*/
+                    x = rows[i].getElementsByTagName("TD")[0];
+                    y = rows[i + 1].getElementsByTagName("TD")[0];
+                    /*check if the two rows should switch place,
+                    based on the direction, asc or desc:*/
+                    if (dir == "asc") {
+                      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                        //if so, mark as a switch and break the loop:
+                        shouldSwitch= true;
+                        break;
+                      }
+                    } else if (dir == "desc") {
+                      if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                        //if so, mark as a switch and break the loop:
+                        shouldSwitch = true;
+                        break;
+                      }
+                    }
+                  }
+                  if (shouldSwitch) {
+                    /*If a switch has been marked, make the switch
+                    and mark that a switch has been done:*/
+                    rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                    switching = true;
+                    //Each time a switch is done, increase this count by 1:
+                    switchcount ++;      
+                  } else {
+                    /*If no switching has been done AND the direction is "asc",
+                    set the direction to "desc" and run the while loop again.*/
+                    if (switchcount == 0 && dir == "asc") {
+                      dir = "desc";
+                      switching = true;
+                    }
+                  }
+                }
+              }
--- Lab_2/Problem-2/script.js
+++ Lab_2/Problem-2/script.js
@@ -1,14 +1,21 @@
 fetch('https://jsonplaceholder.typicode.com/posts')
   .then(result => result.json())
-  .then(data => {data.filter((posts) => { return posts.title.split(" ").length > 6}).map((posts) => {
+	//filters so only posts, returns all posts by title, adds them to a list and counts to make sure there is a length of 6 then maps it to posts
+  .then(json => {json.filter((posts) => { return posts.title.split(" ").length > 6}).map((posts) => {
+	//log it
   console.log(posts.title)});
   
-var wordMap = data.map((posts) => {return posts.body;}).join(" ").split(" ").reduce((wordMap, words) => {
+	//Joins all words together, then splits into a list so we cna count it, reduce returns a single value from all results
+var wordMap = json.map((posts) => {return posts.body;}).join(" ").split(" ").reduce((wordMap, words) => {
 	if (wordMap[words]) {
+		//incremenet through each word
 		wordMap[words] = wordMap[words] + 1;
 	} else {
+		//if only 1 word exsists set it to 1
 		wordMap[words] = 1;
 	}
+	//return everyhting after loops complete
 	return wordMap;}, {});
+	//log it
 console.log(wordMap)});
 
