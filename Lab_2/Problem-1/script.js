1d2693b11fa5b853ea49f6bcd3b0ad301570dd8e
Dylan Heathcote <c19434966@mytudublin.ie>
2022-11-06 14:01:47 +0000

    comments for prob 1 and 2

--- Lab_2/Problem-1/script.js
+++ Lab_2/Problem-1/script.js
@@ -1,14 +1,17 @@
 window.onload = function(){
-
+    //adding click event listener for when a user clicks add contact
     document.getElementById("addcontact").addEventListener("click", () => {
+        //getting the error ID
         document.getElementById("error")
+        //getting the value of the Name, number and email from the input boxes
         var nameText = document.getElementById("name1").value;
         var numberText = document.getElementById("number").value;
         var emailText = document.getElementById("email").value;
-    
+        //getting the id and tag name of the table 
         var table = document.getElementById('table')
         var table = document.getElementsByTagName('table')[0]
 
+        //error checking
         if(!nameText || !numberText || !emailText) {
             displayError("Cannot be blank!")
             return;
@@ -34,31 +37,35 @@ window.onload = function(){
             return;
         }
 
-
+        //inserting a new table row for each entry
         var newRow = table.insertRow(1);
         
+        //inserting a new cell on each part of the table
         var cell1 = newRow.insertCell(0);
         var cell2 = newRow.insertCell(1);
         var cell3 = newRow.insertCell(2);
 
+        //inserting the text into the innerHTML
         cell1.innerHTML = nameText;
         cell2.innerHTML = numberText;
         cell3.innerHTML = emailText;
 
+        //resetting the HTML values to be blank
         name1.value = "";
-		number.value = "";
-		email.value = "";
+		    number.value = "";
+		    email.value = "";
 
-        
     })
 
+    //Displaying the error message function when an error is triggered, it will display whatever specific error.
     function displayError(message) {
         document.getElementById("errormessage").innerHTML = message;
+        //Removes the hidden CSS so the error actually displays.
         document.getElementById("popupmessage").classList.remove("hidden");
     }
-
-	document.getElementById("closeerror").addEventListener("click", () => {
-		document.getElementById("popupmessage").classList.add("hidden");
+        //adds event listener so when the X is clicked, it adds the hidden CSS to hide the error message.
+	      document.getElementById("closeerror").addEventListener("click", () => {
+		    document.getElementById("popupmessage").classList.add("hidden");
 	})
     }
     
--- Lab_2/Problem-2/script.js
+++ Lab_2/Problem-2/script.js
@@ -1,9 +1,9 @@
 fetch('https://jsonplaceholder.typicode.com/posts')
   .then(result => result.json())
-  .then(json => {json.filter((posts) => { return posts.title.split(" ").length > 6}).map((posts) => {
+  .then(data => {data.filter((posts) => { return posts.title.split(" ").length > 6}).map((posts) => {
   console.log(posts.title)});
   
-var wordMap = json.map((posts) => {return posts.body;}).join(" ").split(" ").reduce((wordMap, words) => {
+var wordMap = data.map((posts) => {return posts.body;}).join(" ").split(" ").reduce((wordMap, words) => {
 	if (wordMap[words]) {
 		wordMap[words] = wordMap[words] + 1;
 	} else {
